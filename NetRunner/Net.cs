117fd77592ec6a4e8656b17e8bf269f78df8d389
TheHiddenKnowledge <finney538@gmail.com>
2021-12-29 01:41:35 -0700

    Added saving and loading data

--- NetRunner/Net.cs
+++ NetRunner/Net.cs
@@ -1,7 +1,16 @@
-﻿using MathNet.Numerics.LinearAlgebra;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MathNet.Numerics.Data.Text;
+using MathNet.Numerics.LinearAlgebra;
 using System;
+using System.Collections.Generic;
+using System.IO;
+
+/*
+    NetRunner for C# 
+    Created by Isaiah Finney
+    This is a general purpose library that can create, execute, and optimize neural nets 
+    The algorithm for gradient descent uses momentum to speed up the optimization process 
+*/
+
 namespace NetRunner
 {
     public class Net
@@ -9,7 +18,7 @@ namespace NetRunner
         public int maxInput;
         public int maxOutput;
         public int[] layers;
-        public List<int> totalNeurons= new List<int>();
+        public List<int> totalNeurons = new List<int>();
         public Vector<double> inputs;
         public Vector<double> outputs;
         public List<Matrix<double>> weights = new List<Matrix<double>>();
@@ -18,13 +27,19 @@ namespace NetRunner
         public List<Vector<double>> gradientb = new List<Vector<double>>();
         public List<Matrix<double>> prevgradientw;
         public List<Vector<double>> prevgradientb;
+        // The output of each layer before the sigmoid function is applied 
         public List<Vector<double>> layerout = new List<Vector<double>>();
+        // The output of each layer after the sigmoid function is applied 
         public List<Vector<double>> layersquish = new List<Vector<double>>();
+        // Parameter for how much the sigmoid function squishes layer output 
         public double squish;
+        // Parameters used for gradient descent speed 
         public double alpha;
         public double beta;
         public int successes = 0;
         public int fails = 0;
+        Random random = new Random();
+        // Constructor for the neural net class, where the dimensions of the inputs, outputs, and hidden layers are defined 
         public Net(int inputs, int outputs, int[] layers, double alpha, double beta, double squish)
         {
             maxInput = inputs;
@@ -41,57 +56,217 @@ namespace NetRunner
                 totalNeurons.Add(this.layers[i]);
             }
             totalNeurons.Add(maxOutput);
+        }
+        // Method that generates the weights and biases for the net, must be called after creating the net object 
+        public void generatenet(double minweight, double maxweight, double minbias, double maxbias)
+        {
             for (int i = 0; i < totalNeurons.Count - 1; i++)
             {
-                Matrix<double> tempw = 10 * Matrix<double>.Build.Random(totalNeurons[i + 1], totalNeurons[i]) - 5;
+                // Creating the weight and biases matrices 
+                Matrix<double> tempw = Matrix<double>.Build.Dense(totalNeurons[i + 1], totalNeurons[i]);
+                Vector<double> tempb = Vector<double>.Build.Dense(totalNeurons[i + 1]);
+                for (int j = 0; j < tempw.RowCount; j++)
+                {
+                    tempb[j] = (maxweight - minweight) * random.NextDouble() + minweight;
+                    for (int k = 0; k < tempw.ColumnCount; k++)
+                    {
+                        tempw[j, k] = (maxbias - minbias) * random.NextDouble() + minbias;
+                    }
+                }
                 weights.Add(tempw);
-                Vector<double> tempb = 10 * Vector<double>.Build.Random(totalNeurons[i + 1]) - 5;
                 biases.Add(tempb);
+                // Creating the gradient matrices for the weights and biases 
                 gradientw.Add(Matrix<double>.Build.Dense(totalNeurons[i + 1], totalNeurons[i]));
                 gradientb.Add(Vector<double>.Build.Dense(totalNeurons[i + 1]));
             }
             prevgradientw = gradientw;
             prevgradientb = gradientb;
         }
-        public void RandomGenes()
-        {
-            weights = new List<Matrix<double>>();
-            biases = new List<Vector<double>>();
-            for (int i = 0; i < totalNeurons.Count - 1; i++)
-            {
-                Matrix<double> tempw = 10 * Matrix<double>.Build.Random(totalNeurons[i + 1], totalNeurons[i]) - 5;
-                weights.Add(tempw);
-                Matrix<double> tempb = 10 * Matrix<double>.Build.Random(totalNeurons[i + 1], 1) - 5;
-                weights.Add(tempb);
-            }
-        }
+        // Method for applying the sigmoid function to the input 
         public double sigmoid(double x)
         {
             return 1 / (1 + Math.Pow(Math.E, -x / squish));
         }
+        // Method for getting the derivative of the sigmoid function evaluated at the input 
         public double sigderiv(double x)
         {
-            return Math.Pow(Math.E, x / squish) / Math.Pow((Math.Pow(Math.E, x / squish) + 1), 2) / squish; 
+            double num = Math.Pow(Math.E, x / squish) / Math.Pow((Math.Pow(Math.E, x / squish) + 1), 2) / squish;
+            if (num == double.NaN)
+            {
+                double dz = .00001;
+                return (sigmoid(x + dz) - sigmoid(x) / dz);
+            }
+            else
+            {
+                return num;
+            }
         }
+        // Method for evaluating the neural net with the provided input 
         public Vector<double> In2Out()
         {
             layerout = new List<Vector<double>>();
             layersquish = new List<Vector<double>>();
             Vector<double> current = inputs;
+            Vector<double> currentsquish = inputs;
             layerout.Add(current);
             layersquish.Add(inputs);
-            for(int i = 0; i < weights.Count; i++)
+            // Using matrix multiplication and addition to get each layers output 
+            for (int i = 0; i < weights.Count; i++)
             {
-                current = weights[i]* current + biases[i];
-                Vector<double> currentsquish = Vector<double>.Build.DenseOfVector(current);
-                layerout.Add(current); 
+                current = (weights[i] * current) + biases[i];
+                currentsquish = Vector<double>.Build.DenseOfVector(current);
+                layerout.Add(current);
                 for (int j = 0; j < current.Count; j++)
                 {
                     currentsquish[j] = sigmoid(currentsquish[j]);
                 }
                 layersquish.Add(currentsquish);
             }
-            return current;
+            return currentsquish;
+        }
+        // Method that adjusts the output to match the desired range 
+        public Vector<double> adjustoutput(double maxOut, double minOut)
+        {
+            return (maxOut - minOut) * outputs + minOut;
+        }
+        // Method that saves matrix data to a folder 
+        public void savedata(string path)
+        {
+            Directory.CreateDirectory(path);
+            string filename;
+            string pathname;
+            for (int i = 0; i < weights.Count; i++)
+            {
+                filename = "w" + i.ToString() + ".txt";
+                pathname = Path.Combine(path, filename);
+                DelimitedWriter.Write(pathname, weights[i]);
+                filename = "b" + i.ToString() + ".txt";
+                pathname = Path.Combine(path, filename);
+                Matrix<double> tempb = Matrix<double>.Build.Dense(biases[i].Count, 1);
+                for (int j = 0; j < biases[i].Count; j++)
+                {
+                    tempb[j, 0] = biases[i][j];
+                }
+                DelimitedWriter.Write(pathname, tempb);
+            }
+        }
+        // Method that saves matrix data to a folder 
+        public void loaddata(string path)
+        {
+            string filename;
+            string pathname;
+            for (int i = 0; i < weights.Count; i++)
+            {
+                filename = "w" + i.ToString() + ".txt";
+                pathname = Path.Combine(path, filename);
+                weights[i] = DelimitedReader.Read<double>(pathname, false);
+                filename = "b" + i.ToString() + ".txt";
+                pathname = Path.Combine(path, filename);
+                Matrix<double> tempb = DelimitedReader.Read<double>(pathname, false);
+                for (int j = 0; j < biases[i].Count; j++)
+                {
+                    biases[i][j] = tempb[j, 0];
+                }
+            }
+        }
+        // Method that loads matrix data from a folder 
+
+        // Method that obtains the gradient with respect to the weights and biases 
+        public void getgradient(Vector<double> expected)
+        {
+            List<List<double>> tempg = new List<List<double>>();
+            for (int i = 0; i < weights.Count; i++)
+            {
+                int idx = weights.Count - i - 1;
+                List<double> tempgg = new List<double>();
+                for (int j = 0; j < weights[idx].RowCount; j++)
+                {
+                    // Using multivariable chain rule to get the gradient of the biases 
+                    if (i == 0)
+                    {
+                        gradientb[idx][j] = sigderiv(layerout[idx + 1][j]) * 2 * (layersquish[idx + 1][j] - expected[j]);
+                    }
+                    else
+                    {
+                        gradientb[idx][j] = sigderiv(layerout[idx + 1][j]) * tempg[i - 1][j];
+                    }
+                    for (int k = 0; k < weights[idx].ColumnCount; k++)
+                    {
+                        // Using multivariable chain rule to get the gradient of the weights  
+                        if (i == 0)
+                        {
+                            gradientw[idx][j, k] = layerout[idx][k] * sigderiv(layerout[idx + 1][j]) * 2 * (layersquish[idx + 1][j] - expected[j]);
+                            if (j == 0)
+                            {
+                                tempgg.Add(weights[idx][j, k] * sigderiv(layerout[idx + 1][j]) * 2 * (layersquish[idx + 1][j] - expected[j]));
+                            }
+                            else
+                            {
+                                tempgg[k] += weights[idx][j, k] * sigderiv(layerout[idx + 1][j]) * 2 * (layersquish[idx + 1][j] - expected[j]);
+                            }
+                        }
+                        else
+                        {
+                            gradientw[idx][j, k] = layerout[idx][k] * sigderiv(layerout[idx + 1][j]) * tempg[i - 1][j];
+                            if (j == 0)
+                            {
+                                tempgg.Add(weights[idx][j, k] * sigderiv(layerout[idx + 1][j]) * tempg[i - 1][j]);
+                            }
+                            else
+                            {
+                                tempgg[k] += weights[idx][j, k] * sigderiv(layerout[idx + 1][j]) * tempg[i - 1][j];
+                            }
+                        }
+                    }
+                }
+                tempg.Add(tempgg);
+            }
+        }
+        // Method that gets the average gradient of a set of data and applies it to the weights and biases of the net 
+        public double getset(List<Vector<double>> inputset, List<Vector<double>> expectedset)
+        {
+            // Average gradients for the set of data 
+            List<Matrix<double>> avg_gw = new List<Matrix<double>>();
+            List<Vector<double>> avg_gb = new List<Vector<double>>();
+            // Average error for the set of data 
+            double avg_error = 0;
+            for (int i = 0; i < inputset.Count; i++)
+            {
+                inputs = inputset[i];
+                In2Out();
+                getgradient(expectedset[i]);
+                for (int j = 0; j < weights.Count; j++)
+                {
+                    if (i == 0)
+                    {
+                        avg_gw.Add(gradientw[j]);
+                        avg_gb.Add(gradientb[j]);
+                    }
+                    else
+                    {
+                        avg_gw[j] += gradientw[j];
+                        avg_gb[j] += gradientb[j];
+                    }
+                }
+                // Calculating the average error 
+                double avg_expected = 0;
+                for (int j = 0; j < expectedset[i].Count; j++)
+                {
+                    avg_expected += Math.Pow(layersquish[weights.Count][j] - expectedset[i][j], 2);
+                }
+                avg_expected /= expectedset[i].Count;
+                avg_error += avg_expected;
+            }
+            avg_error /= expectedset.Count;
+            // Descending the gradient using the momentum theorem for neural nets 
+            for (int i = 0; i < weights.Count; i++)
+            {
+                weights[i] -= (beta * prevgradientw[i] + alpha * avg_gw[i]);
+                biases[i] -= (beta * prevgradientb[i] + alpha * avg_gb[i]);
+            }
+            prevgradientb = avg_gb;
+            prevgradientw = avg_gw;
+            return avg_error;
         }
     }
 }
--- NetRunnerDebug/Program.cs
+++ NetRunnerDebug/Program.cs
@@ -1,55 +1,95 @@
-﻿using NetRunner;
+﻿using MathNet.Numerics.LinearAlgebra;
+using NetRunner;
 using System;
-using MathNet.Numerics.LinearAlgebra;
+using System.Collections.Generic;
 namespace NetRunnerDebug
 {
     class Program
     {
+        // This example will take in an array of booleans and then determine the color using a neural net 
         static void Main(string[] args)
         {
-            Net net = new Net(2, 1, new int[] { 1 }, .01, .9, 3);
-            double[] input = new double[] { 1, 1 };
-            net.inputs = Vector<double>.Build.DenseOfArray(input);
-            net.outputs = net.In2Out();
-            for (int i = 0; i < net.weights.Count; i++)
+            // Creating neural net object 
+            Net net = new Net(3, 8, new int[] { 6, 8 }, .0001, .9, 2.5);
+            // Generating a random net to start off from 
+            net.generatenet(-.5, .5, -.5, .5);
+            double error = 1;
+            int i = 0;
+            Random random = new Random();
+            // The net will continue to descend until the error is below the threshold value 
+            while (error > .001)
             {
-                for (int j = 0; j < net.weights[i].RowCount; j++)
+                i += 1;
+                List<Vector<double>> expectedset = new List<Vector<double>>();
+                List<Vector<double>> inputset = new List<Vector<double>>();
+                for (int j = 0; j < 10; j++)
                 {
-                    Console.WriteLine("");
-                    for (int k = 0; k < net.weights[i].ColumnCount; k++)
+                    // Generating RGB boolean inputs
+                    double[] color = new double[] { random.Next(2), random.Next(2), random.Next(2) };
+                    inputset.Add(Vector<double>.Build.DenseOfArray(color));
+                    // Generating expected outputs from the given inputs 
+                    if (color[0] == 1 && color[1] == 0 && color[2] == 0)
                     {
-                        Console.Write(net.weights[i][j, k] + " ");
+                        double[] expected = new double[] { 1, 0, 0, 0, 0, 0, 0, 0 };
+                        expectedset.Add(Vector<double>.Build.DenseOfArray(expected));
+                    }
+                    else if (color[0] == 0 && color[1] == 1 && color[2] == 0)
+                    {
+                        double[] expected = new double[] { 0, 1, 0, 0, 0, 0, 0, 0 };
+                        expectedset.Add(Vector<double>.Build.DenseOfArray(expected));
+                    }
+                    else if (color[0] == 0 && color[1] == 0 && color[2] == 1)
+                    {
+                        double[] expected = new double[] { 0, 0, 1, 0, 0, 0, 0, 0 };
+                        expectedset.Add(Vector<double>.Build.DenseOfArray(expected));
+                    }
+                    else if (color[0] == 1 && color[1] == 1 && color[2] == 0)
+                    {
+                        double[] expected = new double[] { 0, 0, 0, 1, 0, 0, 0, 0 };
+                        expectedset.Add(Vector<double>.Build.DenseOfArray(expected));
+                    }
+                    else if (color[0] == 1 && color[1] == 0 && color[2] == 1)
+                    {
+                        double[] expected = new double[] { 0, 0, 0, 0, 1, 0, 0, 0 };
+                        expectedset.Add(Vector<double>.Build.DenseOfArray(expected));
+                    }
+                    else if (color[0] == 0 && color[1] == 1 && color[2] == 1)
+                    {
+                        double[] expected = new double[] { 0, 0, 0, 0, 0, 1, 0, 0 };
+                        expectedset.Add(Vector<double>.Build.DenseOfArray(expected));
+                    }
+                    else if (color[0] == 1 && color[1] == 1 && color[2] == 1)
+                    {
+                        double[] expected = new double[] { 0, 0, 0, 0, 0, 0, 1, 0 };
+                        expectedset.Add(Vector<double>.Build.DenseOfArray(expected));
+                    }
+                    else if (color[0] == 0 && color[1] == 0 && color[2] == 0)
+                    {
+                        double[] expected = new double[] { 0, 0, 0, 0, 0, 0, 0, 1 };
+                        expectedset.Add(Vector<double>.Build.DenseOfArray(expected));
                     }
                 }
-                Console.WriteLine("");
-            }
-            for (int i = 0; i < net.biases.Count; i++)
-            {
-                Console.WriteLine("");
-                    for (int k = 0; k < net.biases[i].Count; k++)
-                    {
-                        Console.Write(net.biases[i][k] + " ");
-                    }
-            }
-            Console.WriteLine("");
-            for (int i = 0; i < net.layerout.Count; i++)
-            {
-                Console.WriteLine("");
-                for (int j = 0; j < net.layerout[i].Count; j++)
+                error = net.getset(inputset, expectedset);
+                if (i % 500 == 0)
                 {
-                    Console.Write(net.layerout[i][j]+" ");
+                    Console.WriteLine("The error after " + i + " iterations is " + error);
                 }
             }
-            Console.WriteLine("");
-            for (int i = 0; i < net.layersquish.Count; i++)
+            Console.WriteLine("The error after " + i + " iterations is " + error);
+            Console.WriteLine();
+            Vector<double> input;
+            // Testing to see how accurate the neural net is at guessing the colors 
+            List<double[]> colors = new List<double[]>() { new double[] { 1, 0, 0 }, new double[] { 0, 1, 0 }, new double[] { 0, 0, 1 }, new double[] { 1, 1, 0 },
+                new double[] { 1, 0, 1 }, new double[] { 0, 1, 1 }, new double[] { 1, 1, 1 }, new double[] { 0, 0, 0 }, };
+            string[] colornames = { "Red", "Green", "Blue", "Yellow", "Purple", "Cyan", "White", "Black" };
+            for (int k = 0; k < colors.Count; k++)
             {
-                Console.WriteLine("");
-                for (int j = 0; j < net.layersquish[i].Count; j++)
-                {
-                    Console.Write(net.layersquish[i][j] + " ");
-                }
+                input = Vector<double>.Build.DenseOfArray(colors[k]);
+                net.inputs = input;
+                net.outputs = net.In2Out();
+                Console.WriteLine(colornames[net.outputs.MaximumIndex()]);
             }
-            Console.WriteLine("");
+            net.savedata(@"c:\BestNet");
             Console.ReadLine();
         }
     }
